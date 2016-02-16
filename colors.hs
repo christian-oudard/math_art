f9a77de613ca249047f8d251532dd726948808a4
Christian Oudard <christian.oudard@gmail.com>
2016-02-16 10:59:00 -0800

    More iteration on displaying color spaces.

--- colors.hs
+++ colors.hs
@@ -15,15 +15,31 @@ import System.Random.MWC (GenIO, createSystemRandom, uniformR)
 
 main = do
   gen <- createSystemRandom
-  colors <- genColors 3000 gen
-  let sortedColors = sortBy cmpColor colors
+  colors <- genColors 10000 gen
   display
     (InWindow "" (round screenWidth, round screenHeight) (0,0))
-    (toScreen black)
-    (colorStar sortedColors)
+    (toScreen gray)
+    (colorStar $ sortLightness colors)
   return ()
 
-cmpColor = compare `on` (\c -> (lightness c, - chroma c))
+-- Constants
+screenWidth, screenHeight, pxPerUnit :: Float
+screenWidth = 1600
+screenHeight = 900
+pxPerUnit = 3
+scaling = Scale pxPerUnit pxPerUnit 
+
+-- Colors
+gray = CIELCH 50 0 0
+
+rgb :: Integer -> Integer -> Integer -> CIELCH Double
+rgb r g b = fromRGB $ RGB r g b
+
+lightness (CIELCH l _ _) = realToFrac l / 100
+chroma (CIELCH _ c _) = realToFrac c
+hue (CIELCH _ _ h) = degToRad $ realToFrac h
+blueyellow c = chroma c * sin (hue c)
+greenred c = chroma c * cos (hue c)
 
 genColors n gen = sequence $ replicate n $ genColor gen
 genColor gen = do
@@ -31,30 +47,8 @@ genColor gen = do
   g <- genComponent gen
   b <- genComponent gen
   return $ rgb (fromIntegral r) (fromIntegral g) (fromIntegral b)
-
 genComponent :: GenIO -> IO Int
 genComponent g = uniformR (0,255) g
-
--- Constants
-screenWidth, screenHeight, pxPerUnit :: Float
-screenWidth = 1600
-screenHeight = 900
-pxPerUnit = 2
-scaling = Scale pxPerUnit pxPerUnit 
-
--- Colors
-black = rgb 0 0 0
-gray = CIELCH 50 0 0
-white = rgb 255 255 255
-red = rgb 255 0 0
-green = rgb 0 255 0
-blue = rgb 0 0 255
-cyan = rgb 0 255 255
-magenta = rgb 255 0 255
-yellow = rgb 255 255 0
-
-rgb :: Integer -> Integer -> Integer -> CIELCH Double
-rgb r g b = fromRGB $ RGB r g b
 
 toScreen :: CIELCH Double -> Color
 toScreen color = toScreenAlpha color 1
@@ -65,26 +59,44 @@ toScreenAlpha color alpha = makeColor r g b alpha
     g = realToFrac g' / 255
     b = realToFrac b' / 255
 
-
-lightness (CIELCH l _ _) = realToFrac l
-chroma (CIELCH _ c _) = realToFrac c
-hue (CIELCH _ _ h) = degToRad $ realToFrac h
-
 tau = 2*pi
 degToRad d = tau * d / 360
 radToDeg r = 360 * r / tau
 
+-- Drawing
+
 colorStar :: [CIELCH Double] -> Picture
 colorStar colors = scaling $ Pictures $ map colorDot colors
 
-colorDot c = Color (toScreenAlpha c 0.3) $ Translate x y $ circleSolid 6
+colorDot c = Color (toScreenAlpha c 0.8) $ Translate x y $ circleSolid 6
+  where (x, y) = colorPositionPolar c
+
+colorLine c = Color (toScreenAlpha c 0.6) $ Line [(0, 0), (x, y)]
   where (x, y) = polar (hue c) (chroma c)
 
-colorLine c = Color (toScreenAlpha c 0.6) $ Line [(x/2, y/2), (x, y)]
-  where (x, y) = polar (hue c) (chroma c)
+colorPositionPolar c = polar (hue c) (chroma c)
+colorPositionSide c = (-greenred c, -100 + 200 * lightness c)
 
-origin :: (Float, Float)
-origin = (0, 0)
+sortLightness = sortBy cmp
+  where cmp = compare `on` (\c -> (lightness c, chroma c))
+sortBlueYellow = sortBy cmp
+  where cmp = compare `on` (\c -> -blueyellow c)
+
+filterColorsLightness = filter pred
+  where
+    pred c =
+      ( lightness c > 0.3
+      && lightness c < 0.8
+      )
+
+filterColorsRing = filter pred
+  where
+    pred c =
+      ( lightness c > 0.6
+      && lightness c < 0.7
+      && chroma c > 20
+      && chroma c < 43
+      )
 
 polar :: Float -> Float -> (Float, Float)
 polar theta r = (r * cos theta, r * sin theta)
