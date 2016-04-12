9607ece7bd4e9ae4ee7327a06dad0b752a9db7a3
Christian Oudard <christian.oudard@gmail.com>
2016-04-11 18:22:08 -0700

    Four dimensional rotating octaplex.

--- FourDee.hs
+++ FourDee.hs
@@ -3,11 +3,13 @@ module FourDee
   , vec
   , vecToCoord
   , hyperCube
-  , flatten2d
+  , octaplex
   , rot2d
   , rot3d
   , rot4d
   , rotNd
+  , rotation3d
+  , rotation4d
   , embedMatrix
   , switchAxes
   , tau
@@ -36,12 +38,54 @@ rot3d theta = embedMatrix (rot2d theta) (identity 3)
 rot4d theta = embedMatrix (rot2d theta) (identity 4)
 rotNd n theta = embedMatrix (rot2d theta) (identity n)
 
+rotation3d t' = let t = t'*1 in (
+  (rot3d $ t * tau/61)
+  * (switchAxes 2 3 $ rot3d $ t * tau/73) 
+  * (switchAxes 1 2 $ switchAxes 2 3 $ rot3d $ t * tau/97) 
+  )
+
+rotation4d t' = let t = t'/2 in (
+  (rot4d $ t * tau/61)
+  * (switchAxes 2 3 $ rot4d $ t * tau/73) 
+  * (switchAxes 2 4 $ rot4d $ t * tau/83) 
+  * (switchAxes 1 2 $ switchAxes 2 3 $ rot4d $ t * tau/97) 
+  * (switchAxes 1 2 $ switchAxes 2 4 $ rot4d $ t * tau/103) 
+  * (switchAxes 1 3 $ switchAxes 2 4 $ rot4d $ t * tau/117) 
+  )
+
+
 switchAxes a1 a2 = switchRows a1 a2 . switchCols a1 a2
 
 hyperCube :: Int -> [Vec]
 hyperCube n = map vec $ crossProduct $ replicate n [1, -1]  
 
-flatten2d = submatrix 1 2 1 1
+
+octaplex = map vec $
+  [ [2, 0, 0, 0]
+  , [-2, 0, 0, 0]
+  , [0, 2, 0, 0]
+  , [0, -2, 0, 0]
+  , [0, 0, 2, 0]
+  , [0, 0, -2, 0]
+  , [0, 0, 0, 2]
+  , [0, 0, 0, -2]
+  , [1, 1, 1, 1]
+  , [1, 1, 1, -1]
+  , [1, 1, -1, 1]
+  , [1, 1, -1, -1]
+  , [1, -1, 1, 1]
+  , [1, -1, 1, -1]
+  , [1, -1, -1, 1]
+  , [1, -1, -1, -1]
+  , [-1, 1, 1, 1]
+  , [-1, 1, 1, -1]
+  , [-1, 1, -1, 1]
+  , [-1, 1, -1, -1]
+  , [-1, -1, 1, 1]
+  , [-1, -1, 1, -1]
+  , [-1, -1, -1, 1]
+  , [-1, -1, -1, -1]
+  ]
 
 -- TODO: a more explicit cross product implementation
 crossProduct :: [[a]] -> [[a]]
--- cuberotate.hs
+++ cuberotate.hs
@@ -8,22 +8,14 @@ main = do
 
 frame t = frame' (realToFrac t)
 frame' :: Double -> Picture
-frame' t = Scale 100 100 $ Color white $ drawPoints $ geometry
+frame' t = Scale 200 200 $ drawPoints $ geometry
   where
-    geometry = map flatten2d $ map (rotation *) $ hyperCube 4
+    geometry = map (rotation *) $ octaplex
     rotation = rotation4d t
 
-rotation4d t = (
-  (rot4d $ t * tau/60)
-  * (switchAxes 2 3 $ rot4d $ t * tau/73) 
-  * (switchAxes 1 3 $ rot4d $ t * tau/97) 
-  * (switchAxes 2 4 $ rot4d $ t * tau/53) 
-  * (switchAxes 1 4 $ rot4d $ t * tau/117) 
-  )
-
-
 drawPoints :: [Vec] -> Picture
-drawPoints points = Pictures $ map (\(x, y) ->Translate x y $ filledCircle 0.1) $ map vecToCoord points
-    
-filledCircle :: Float -> Picture
-filledCircle r = ThickCircle (r/2) r
+drawPoints points = Pictures $ map drawPoint points
+
+drawPoint p =
+  let (x, y) = vecToCoord p
+   in Translate x y $ Color white $ circleSolid 0.03
